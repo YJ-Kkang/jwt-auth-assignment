b8144381a9180da4d0e675e76008ffae251f5d3f
emotion-test <a29ei1991@gmail.com>
2025-04-19 14:35:20 +0900

    refactor: SigninResponseDto에서 접두사 Bearer 제거, SwaggerConfig에서 접두사 붙이는 것으로 변경

--- src/main/java/com/yj/jwtauth/domain/user/dto/response/SigninResponseDto.java
+++ src/main/java/com/yj/jwtauth/domain/user/dto/response/SigninResponseDto.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class SigninResponseDto {
 
-	@Schema(description = "JWT 토큰", example = "Bearer eyJh7...")
+	@Schema(description = "JWT 토큰", example = "eyJh7... | SwaggerConfig가 Bearer를 붙여줘서 최종적으로 `Authorization` 헤더에 `Bearer ` 접두사가 붙음.")
 	private final String token;
 
 	public SigninResponseDto(String token) {
